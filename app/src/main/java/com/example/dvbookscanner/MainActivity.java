2d3c374180999b5d2a5b597dddd0511d4832038c
chinonrg <nicolas.rios.nr@gmail.com>
2019-09-09 22:07:56 -0300

    Interfaz cambiada. Falta solucionar null pointer exception en la imagen

--- app/src/main/java/com/example/dvbookscanner/MainActivity.java
+++ app/src/main/java/com/example/dvbookscanner/MainActivity.java
@@ -1,45 +1,72 @@
 package com.example.dvbookscanner;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
 import android.provider.MediaStore;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.EditText;
 import android.widget.Toast;
+
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.*;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnCapture;
-    private ImageView imgCapture;
+    private EditText nom, cantidad;
+    private int cant = 0;
     private int count = 0;
     private static final int Image_Capture_Code = 1;
+    private DatabaseReference mDatabaseShare;
+    private StorageReference mStorageShare;
+    private Uri mFileUri;
+    private String carpeta;
+    private ProgressDialog mProgress;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnCapture = findViewById(R.id.btnTakePicture);
+        mDatabaseShare = FirebaseDatabase.getInstance().getReference().child("SharedMedia");
+        mStorageShare = FirebaseStorage.getInstance().getReference();
+        mDatabaseShare.keepSynced(true);
+        mProgress = new ProgressDialog(this);
         btnCapture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                cInt.putExtra(MediaStore.EXTRA_OUTPUT, "scan" + count + ".jpg");
-                cInt.putExtra("android.intent.extra.quickCapture", true);
-                startActivityForResult(cInt,Image_Capture_Code);
+                sacarFoto();
             }
         });
+    }
+    private void sacarFoto(){
+        Intent cInt = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        cInt.putExtra(MediaStore.EXTRA_OUTPUT, "scan" + count + ".jpg");
+        cInt.putExtra("android.intent.extra.quickCapture", true);
+        nom = (EditText)findViewById(R.id.nombreCarpeta);
+        cantidad = (EditText)findViewById(R.id.cantidad);
+        cant = Integer.parseInt(cantidad.getText().toString());
+        carpeta = nom.getText().toString();
+        startActivityForResult(cInt,Image_Capture_Code);
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == Image_Capture_Code) {
             if (resultCode == RESULT_OK) {
-                Bitmap bp = (Bitmap) data.getExtras().get("data");
-                imgCapture.setImageBitmap(bp);
-                count++;
+                while(cant > count) {
+                    sacarFoto();
+                }
             } else if (resultCode == RESULT_CANCELED) {
-                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show();
+                Toast.makeText(this, "Cancelado", Toast.LENGTH_LONG).show();
+                finish();
             }
+            finish();
         }
     }
 }
